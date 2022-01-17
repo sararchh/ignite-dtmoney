ad34076377a7f8840728e6664a4b5410c11c0d33
Sara Rocha <sararocha@MacBook-Air-de-Codefique.local>
2022-01-17 20:37:46 -0300

    modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,27 @@
-import { useState } from 'react';
+import Modal from 'react-modal';
 import logoImg from '../../assets/logo.svg';
+
 import { Container, Content } from './styles';
 
-export function Header() {
-  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen ] = useState(false);
+  interface HeaderProps {
+    onOpenNewTransactionModal: () => void;
+  }
 
-
+export function Header({onOpenNewTransactionModal} : HeaderProps) {
+ 
 
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dt money" />
-        <button type="button">
+        <button
+          type="button"
+          onClick={onOpenNewTransactionModal}
+        >
           Nova Transação
         </button>
+
+       
       </Content>
     </Container>
   )
